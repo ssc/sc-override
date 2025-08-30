27968d888cd73d96f7f720a867fac085f6f67b12
Short Circuits Team <234322+ssc@users.noreply.github.com>
2025-08-29 21:03:56 -0700

    [lucas + kuba] added in button b for jiggle and fixed autons

--- src/main.py
+++ src/main.py
@@ -11,15 +11,17 @@
 MAX_SPEED = 0
 # Library imports
 from vex import *
+import sys
 
-# Brain should be defined by default
+# Brain should be defined by defaultF
 brain=Brain()
 brain=Brain()
 controller = Controller(ControllerType.PRIMARY)
 controller2 = Controller(ControllerType.PARTNER)
 controllerMode = 0
 auton = 0
-
+hopper_running = 0
+b_was_pressed = 0
 brain.screen.print("Hello V5")
 # Create the left Motors and group them under the
 # MotorGroup "left_motors".
@@ -36,17 +38,35 @@ right_motors = MotorGroup(right_motor_a, right_motor_b)
 # 1 is the circumference, 2 is the distance on a single axle between two wheels, 3 is the distance between the two axles, 4 is the unit of measurement
 # DriveTrain class.
 drivetrain = DriveTrain(left_motors, right_motors, 330, 335, 231, MM, 1)
+def hopper_pickup():
+    SCALE_VALUE = 0.6
+    global MAX_SPEED
 
 
+
+    drivetrain.turn_for(RIGHT, 20 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    
+    drivetrain.turn_for(LEFT, 20 * SCALE_VALUE, DEGREES, 25, PERCENT)
+
+    drivetrain.drive_for(FORWARD, 10, MM, 50, PERCENT)
+
+
+    
+    
 def auton_funct():
     #was getting annoying by accadntily hitting the wrong button
-    return
+    
     global MAX_SPEED
+    
     # Move the Drivetrain forward 1000 mm at 50% speed.
     #drivetrain.drive_for(FORWARD, 500, MM, 25, PERCENT)
     # Turn the Drivetrain left 90 degrees at 50% speed.
 
     SCALE_VALUE = 0.6
+    first_intake = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
+    basket_intake_motor = Motor(Ports.PORT15, GearSetting.RATIO_18_1, False)
+    toprack = Motor(Ports.PORT17, GearSetting.RATIO_18_1, False)
+
     #drivetrain.turn_for(LEFT, 90 * SCALE_VALUE, DEGREES, 25, PERCENT)
     # Move the Drivetrain backward 500 mm at 50% speed.
     #drive in a square
@@ -54,18 +74,45 @@ def auton_funct():
     #     drivetrain.drive_for(FORWARD, 200, MM, 25, PERCENT)
     #     drivetrain.turn_for(LEFT, 90 * SCALE_VALUE, DEGREES, 25, PERCENT)
     # start of 15 second auton section
-    drivetrain.drive_for(FORWARD, 200, MM, 25, PERCENT)
-    drivetrain.turn_for(LEFT, 90 * SCALE_VALUE, DEGREES, 25, PERCENT)
-    drivetrain.drive_for(FORWARD, 200, MM, 25, PERCENT)
-    drivetrain.turn_for(RIGHT, 10 * SCALE_VALUE, DEGREES, 25, PERCENT)
-    drivetrain.drive_for(FORWARD, 2000, MM, 25, PERCENT)
-    drivetrain.turn_for(LEFT, 100 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    first_intake.spin(FORWARD, 75, PERCENT)
+    basket_intake_motor.spin(REVERSE, 100, PERCENT)
+    drivetrain.drive_for(FORWARD, 450, MM, 25, PERCENT)
+    drivetrain.turn_for(RIGHT, 30 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    drivetrain.drive_for(FORWARD, 300, MM, 25, PERCENT)
+    drivetrain.turn_for(LEFT, 30 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    drivetrain.drive_for(FORWARD, 550, MM, 20, PERCENT)
+    wait(2, SECONDS)
+    basket_intake_motor.stop()
+    first_intake.stop()
+    drivetrain.drive_for(REVERSE, 620, MM, 25, PERCENT)
+    drivetrain.turn_for(RIGHT, 90 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    drivetrain.drive_for(FORWARD, 700, MM, 25, PERCENT)
+    drivetrain.turn_for(RIGHT, 95 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    drivetrain.drive_for(REVERSE, 90, MM, 25, PERCENT)
+    first_intake.spin(FORWARD, 75, PERCENT)
+    basket_intake_motor.spin(FORWARD, 100, PERCENT)
+    brain.screen.print("we made it")
+    toprack.spin(REVERSE, 100, PERCENT)
+    wait(10, SECONDS)
+    brain.screen.print("we made it 2")
+    basket_intake_motor.stop()
+    first_intake.stop()
+    toprack.stop()
+    brain.screen.print("we made it3")
+    # drivetrain.turn_for(LEFT, 90 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    # drivetrain.drive_for(FORWARD, 200, MM, 25, PERCENT)
+    # drivetrain.turn_for(RIGHT, 10 * SCALE_VALUE, DEGREES, 25, PERCENT)
+    # drivetrain.drive_for(FORWARD, 2000, MM, 25, PERCENT)
+    # drivetrain.turn_for(LEFT, 100 * SCALE_VALUE, DEGREES, 25, PERCENT)
 
     
 def drive_task():
     drive_left = 0
     drive_right = 0
     global MAX_SPEED
+    global hopper_running
+    
+    #global b_was_pressed
     MAX_SPEED= 40
     first_intake = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
     basket_intake_motor = Motor(Ports.PORT15, GearSetting.RATIO_18_1, False)
@@ -104,12 +151,13 @@ def drive_task():
         # Three values, max, 0 and -max.
         brain.screen.print(MAX_SPEED)
         if controllerMode == 0:
+   
             control_A  = (controller.buttonA.pressing() - controller.buttonY.pressing()) * MAX_SPEED
             control_l1  = (controller.buttonL1.pressing() - controller.buttonL2.pressing()) * MAX_SPEED
             control_r1  = (controller.buttonR1.pressing() - controller.buttonR2.pressing()) * MAX_SPEED
-            control_r2  = (controller.buttonR2.pressing()) * MAX_SPEED
-            control_Y  = (controller.buttonY.pressing() - controller.buttonA.pressing()) * MAX_SPEED
-            control_l2  = (controller.buttonL2.pressing() - controller.buttonL1.pressing()) * MAX_SPEED
+            #control_r2  = (controller.buttonR2.pressing()) * MAX_SPEED
+            #control_Y  = (controller.buttonY.pressing() - controller.buttonA.pressing()) * MAX_SPEED
+            #control_l2  = (controller.buttonL2.pressing() - controller.buttonL1.pressing()) * MAX_SPEED
             #control_r2  = (controller.buttonA.pressing() - controller.buttonB.pressing()) * MAX_SPEED
         else:
             # Arcade control
@@ -125,14 +173,16 @@ def drive_task():
 
 
 
-        if controller.buttonB.pressing():
+
+     
+        #if controller.buttonB.pressing():
             # Toggle controller mode
-            brain.screen.clear_screen()
-            controllerMode = (controllerMode + 1) % 2
-            if controllerMode == 0:
-                brain.screen.print("drive")
-            else:
-                brain.screen.print("arm")
+            # brain.screen.clear_screen()
+            # controllerMode = (controllerMode + 1) % 2
+            # if controllerMode == 0:
+            #     brain.screen.print("drive")
+            # else:
+            #     brain.screen.print("arm")
 
 
         if controller.buttonX.pressing():
@@ -145,6 +195,21 @@ def drive_task():
             auton_funct()
             auton = 0
             pass    
+
+
+        if controller.buttonB.pressing():
+            # Toggle controller mode
+            brain.screen.clear_screen()
+
+            hopper_running = 1
+
+
+        if hopper_running == 1:
+            hopper_pickup()
+            hopper_running = 0
+
+  
+
 
 
         if controller.buttonX.pressing():
@@ -218,10 +283,12 @@ def drive_task():
             right_motor_a.spin(FORWARD)
             right_motor_b.spin(FORWARD)
         # Claw and Arm motors
+        
+        
 
 
-        first_intake.spin(FORWARD, control_r1.axis1, PERCENT)
-        basket_intake_motor.spin(FORWARD, control_r1 * 10, PERCENT)
+        first_intake.spin(FORWARD, control_l1 * 10, PERCENT)
+        basket_intake_motor.spin(FORWARD, control_r1 * 10, PERCENT)    
         toprack.spin(REVERSE, control_A * 10, PERCENT)
 
         # No need to run too fast
